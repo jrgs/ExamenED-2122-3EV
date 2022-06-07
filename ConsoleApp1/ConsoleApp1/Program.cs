b383ea87b67966e093f8129e2c218d29651b8ead
Javier <marco12853@iesmarenostrum.com>
2022-06-07 19:35:23 +0200

    Cambios en el examen 2122

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,41 +9,96 @@ namespace ConsoleApp1
     // Clase bill
     // Dada una cantidad, la desglosa en billetes de 50, 20, 10
     //
-    public class bill
+    public class Billetes
     {
-        // numero de billetes de 50, 20, 10
-        public int C50, C20, C10;
+        // numero de billetes de 50, 20, 10 JSM2122
+        /// <summary>
+        /// Propiedades privadas de cada tipo de billete
+        /// </summary>
+        private int billetes50;
+        private int billetes20;
+        private int billetes10;
 
-        // constructor vacio, inicializa los atributos
-        public bill()
-        {
-            C50 = 0;
-            C20 = 0;
-            C10 = 0;
+        /// <summary>
+        /// getter y setter para acceder al valor de Billetes 50
+        /// </summary>
+        public int Billetes50 
+        { 
+            get => billetes50; 
+            set => billetes50 = value; 
         }
 
-        // este método permite cambiar la cantidad total y recalcular
-        public void establecercant(int c)
+        /// <summary>
+        /// getter y setter para acceder al valor de Billetes 20
+        /// </summary>
+        public int Billetes20 
+        { 
+            get => billetes20; 
+            set => billetes20 = value; 
+        }
+        /// <summary>
+        /// getter y setter para acceder al valor de Billetes 10
+        /// </summary>
+        public int Billetes10 
+        { 
+            get => billetes10; 
+            set => billetes10 = value; 
+        }
+        
+        // constructor vacio, inicializa los atributos
+        /// <summary>
+        /// Inicializa la cantidad de billetes de donde empieza a contar los billetes
+        /// </summary>
+        public Billetes()
         {
-            if ((c % 10) != 0)
+            Billetes50 = 0;
+            Billetes20 = 0;
+            Billetes10 = 0;
+        }
+        
+
+        // este método permite cambiar la cantidad total y recalcular
+        /// <summary>
+        /// Establece la cantidad de billetes necesarios para una cantidad
+        /// </summary>
+        /// <param name="cantidad"></param>
+        public void EstablecerCantidad(int cantidad)
+        {
+            cantidad = CalcularCantidad(cantidad);
+
+        }
+        /// <summary>
+        /// Metodo que calcula la cantidad de billetes de cada tipo que se necesitas para una cantidad
+        /// </summary>
+        /// <param name="cantidad">cantidad de billetes en numero entero</param>
+        /// <returns>Devuelve la cantidad de billetes</returns>
+        private int CalcularCantidad(int cantidad)
+        {
+            const int cincuenta = 50;
+            const int veinte = 20;
+            const int diez = 10;
+
+            if ((cantidad % diez) != 0)
+            {
                 throw new ArgumentOutOfRangeException();
+            }
+            if ((cantidad >= cincuenta))
+            {
+                Billetes50 = (cantidad / cincuenta);
+                cantidad = cantidad - (Billetes50 * cincuenta);
+            }
+            if ((cantidad >= veinte))
+            {
+                Billetes20 = (cantidad / veinte);
+                cantidad = cantidad - (Billetes20 * veinte);
+            }
+            if ((cantidad >= diez))
+            {
+                Billetes10 = (cantidad / diez);
+                cantidad = cantidad - (Billetes10 * diez);
+            }
 
-            if ((c >= 50))
-            {
-                C50 = (c / 50);
-                c = c - (C50 * 50);
-            }
-            if ((c >= 20))
-            {
-                C20 = (c / 20);
-                c = c - (C20 * 20);
-            }
-            if ((c >= 10))
-            {
-                C10 = (c / 10);
-                c = c - (C10 * 10);
-            }
-
+            return cantidad;
         }
     }
 
@@ -52,29 +107,36 @@ namespace ConsoleApp1
         // El siguiente programa calcula la cantidad necesaria de billetes de
         // cada tipo para una cantidad dada.
         // Sólo admite billetes de 50, 20 y 10. Si no es así, se produce una excepción
+        /// <summary>
+        /// Cantidad de billetes de cada tipo
+        /// </summary>
+        /// 
         static void Main(string[] args)
         {
-            int cant;
-            bill b = new bill();
-
+            int cantidad;
             string linea;
+
+            Billetes billetes = new Billetes();
+
             Console.Write("Introduzca una cantidad: ");
+            
             linea = Console.ReadLine();
-            cant = int.Parse(linea);
+            cantidad = int.Parse(linea);
 
             try
             {
-                b.establecercant(cant);
-                Console.WriteLine("BILLETES DE 50 : " + b.C50);
-                Console.WriteLine("BILLETES DE 20 : " + b.C20);
-                Console.WriteLine("BILLETES DE 10 : " + b.C10);
+                billetes.EstablecerCantidad(cantidad);
+                Console.WriteLine("BILLETES DE 50 : " + billetes.Billetes50);
+                Console.WriteLine("BILLETES DE 20 : " + billetes.Billetes20);
+                Console.WriteLine("BILLETES DE 10 : " + billetes.Billetes10);
             }
             catch
             {
                 Console.WriteLine("Cantidad no válida.");
             }
 
-            Console.Write("Pulse una Tecla."); Console.ReadLine();
+            Console.Write("Pulse una Tecla."); 
+            Console.ReadLine();
         }
     }
 }
--- ConsoleApp1/UnitTestProject1/UnitTest1.cs
+++ ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -12,8 +12,49 @@ namespace UnitTestProject1
         {
             int cantidad = 280;
             int billetes50 = 4;
-            int billetes20 = 1;
-            int billetes10 = 1;
+            int billetes20 = 4;
+            int billetes10 = 0;
+
+            int billetesCount = 8;
+
+            Billetes billetes = new Billetes();
+
+            int billetesExpected = 8;
+            int billetesExpected50 = 4;
+            int billetesExpected20 = 4;
+            int billetesExpected10 = 0;
+
+            Assert.AreEqual(billetesExpected, billetesCount, 0.001, "La cantidad esta mal");
+            Assert.AreEqual(billetesExpected50, billetes50, 0.001, "La cantidad de 50 esta mal");
+            Assert.AreEqual(billetesExpected20, billetes20, 0.001, "La cantidad de 20 esta mal");
+            Assert.AreEqual(billetesExpected10, billetes10, 0.001, "La cantidad de 10 esta mal");
+
+
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            int cantidad = 9;
+
+            Billetes billetes = new Billetes();
+
+            try
+            {
+                
+                int cantidadExpected = 10;
+                cantidad = cantidadExpected;
+                
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                StringAssert.Contains(e.cantidad, "la cantidad esta mal");
+            }
+
+            
+
+
         }
     }
 }
