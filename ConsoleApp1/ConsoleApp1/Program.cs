210ff520ecc8135aba254050f3997e601f086ad9
DMP2121/1 <moya20818@iesmarenostrum.com>
2022-06-07 20:08:29 +0200

    DMP2122Examen

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,44 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace DMP2122NS
 {
-    // Clase bill
+    /// <summary>
+    ///  Dada una cantidad, la desglosa en billetes de 50, 20, 10
+    /// </summary>
+    // Clase billetes
     // Dada una cantidad, la desglosa en billetes de 50, 20, 10
     //
-    public class bill
+    public class billetes
     {
         // numero de billetes de 50, 20, 10
-        public int C50, C20, C10;
+        public int billetes50, billetes20, billetes10;
 
+        /// <summary>
+        /// Se inicializan las variables a0
+        /// </summary>
         // constructor vacio, inicializa los atributos
-        public bill()
+        public billetes()
         {
-            C50 = 0;
-            C20 = 0;
-            C10 = 0;
+            billetes50 = 0;
+            billetes20 = 0;
+            billetes10 = 0;
         }
 
-        // este método permite cambiar la cantidad total y recalcular
-        public void establecercant(int c)
-        {
-            if ((c % 10) != 0)
-                throw new ArgumentOutOfRangeException();
+        public const string CantidadNoValida = "La cantidad introducida no es válida";
 
-            if ((c >= 50))
+        /// <summary>
+        /// este método permite cambiar la cantidad total y recalcular
+        /// </summary>
+        /// <param name="cantidad">cantidad de dinero que se calcula y permite saber cuántos billetes de cada tipo hay y recalcula  lo que sobra</param>
+        /// <exception cref="ArgumentOutOfRangeException">lanza una excepcion si la cantidad  no es múltiplo de 10</exception>
+
+
+
+        // este método permite cambiar la cantidad total y recalcular
+        public void EstablecerCantidad(int cantidad)
+        {
+            if ((cantidad % 10) != 0)
+                throw new ArgumentOutOfRangeException("cantidad", cantidad, CantidadNoValida);
+      
+       
+            if ((cantidad >= 50))
             {
-                C50 = (c / 50);
-                c = c - (C50 * 50);
+                billetes50 = (cantidad / 50);
+                cantidad = cantidad - (billetes50 * 50);
             }
-            if ((c >= 20))
+            if ((cantidad >= 20))
             {
-                C20 = (c / 20);
-                c = c - (C20 * 20);
+                billetes20 = (cantidad / 20);
+                cantidad = cantidad - (billetes20 * 20);
             }
-            if ((c >= 10))
+            if ((cantidad >= 10))
             {
-                C10 = (c / 10);
-                c = c - (C10 * 10);
+                billetes10 = (cantidad / 10);
+                cantidad = cantidad - (billetes10 * 10);
             }
 
         }
@@ -54,20 +71,20 @@ namespace ConsoleApp1
         // Sólo admite billetes de 50, 20 y 10. Si no es así, se produce una excepción
         static void Main(string[] args)
         {
-            int cant;
-            bill b = new bill();
+            int cantidad;
+            billetes b = new billetes();
 
             string linea;
             Console.Write("Introduzca una cantidad: ");
             linea = Console.ReadLine();
-            cant = int.Parse(linea);
+            cantidad = int.Parse(linea);
 
             try
             {
-                b.establecercant(cant);
-                Console.WriteLine("BILLETES DE 50 : " + b.C50);
-                Console.WriteLine("BILLETES DE 20 : " + b.C20);
-                Console.WriteLine("BILLETES DE 10 : " + b.C10);
+                b.EstablecerCantidad(cantidad);
+                Console.WriteLine("BILLETES DE 50 : " + b.billetes50);
+                Console.WriteLine("BILLETES DE 20 : " + b.billetes20);
+                Console.WriteLine("BILLETES DE 10 : " + b.billetes10);
             }
             catch
             {
---
+++ ConsoleApp1/UnitTestProject1/billetesTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DMP2122NS;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class billetesTest
+    {
+        public const string CantidadNoValida = "La cantidad introducida no es válida";
+        [TestMethod]
+        public void PruebaCantidadValida()
+        {
+            billetes prueba = new billetes();
+            int cantidad = 280;
+            int billetes50 = 5;
+            int billetes20 = 14;
+            int billetes10 = 28;
+
+            prueba.EstablecerCantidad(cantidad);
+
+            Console.WriteLine("BILLETES DE 50 : " + billetes50);
+            Console.WriteLine("BILLETES DE 20 : " + billetes20);
+            Console.WriteLine("BILLETES DE 10 : " + billetes10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException),CantidadNoValida)]
+       public void PruebaValoresFueraRango()
+        {    
+            int cantidad = -5;
+            billetes prueba = new billetes();
+            prueba.EstablecerCantidad(cantidad);
+        }
+
+        [TestMethod]
+        public void PruebaFueraRangoTryCatch()
+        {
+            int cantidad = -5;
+            billetes prueba = new billetes();
+            try
+            {
+                prueba.EstablecerCantidad(cantidad);
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                StringAssert.Contains(e.Message, CantidadNoValida);
+            }
+        }
+    }
+}
