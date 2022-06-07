2cc3f044bdf5a669c5cd33d03ffe03fd5c193603
Agustin <perez19917@iesmarenostrum.com>
2022-06-07 19:47:21 +0200

    Examen Agustin Perez

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,73 +1,123 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
+    using System;
+
     // Clase bill
     // Dada una cantidad, la desglosa en billetes de 50, 20, 10
-    //
-    public class bill
+    // Agustin Perez 2122
+    /// <summary>
+    /// Defines the <see cref="Bill" />.
+    /// </summary>
+    public class Bill
     {
         // numero de billetes de 50, 20, 10
-        public int C50, C20, C10;
+        /// <summary>
+        /// Defines the b50, b20, b10.
+        /// </summary>
+        private int b50, b20, b10;
+
+        /// <summary>
+        /// Gets the mBillete50.
+        /// </summary>
+        public int mBillete50
+        {
+            get { return b50; }
+        }
+
+        /// <summary>
+        /// Gets the mBillete20.
+        /// </summary>
+        public int mBillete20
+        {
+            get { return b20; }
+        }
+
+        /// <summary>
+        /// Gets the mBillete10.
+        /// </summary>
+        public int mBillete10
+        {
+            get { return b10; }
+        }
 
         // constructor vacio, inicializa los atributos
-        public bill()
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Bill"/> class.
+        /// </summary>
+        public Bill()
         {
-            C50 = 0;
-            C20 = 0;
-            C10 = 0;
+            b50 = 0;
+            b20 = 0;
+            b10 = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Bill"/> class.
+        /// </summary>
+        /// <param name="cant">The cant<see cref="int"/>.</param>
+        public Bill(int cant)
+        {
+            establecerCantidad(cant);
         }
 
         // este método permite cambiar la cantidad total y recalcular
-        public void establecercant(int c)
+        /// <summary>
+        /// Funcion para separar una cantidad en billetes de 50, 20 y 10.
+        /// <remark>Minimo 10</remark>
+        /// </summary>
+        /// <param name="cantidadTotal">The cantidadTotal<see cref="int"/>.</param>
+        private void establecerCantidad(int cantidadTotal)
         {
-            if ((c % 10) != 0)
+            if ((cantidadTotal % 10) != 0)
+            {
                 throw new ArgumentOutOfRangeException();
-
-            if ((c >= 50))
-            {
-                C50 = (c / 50);
-                c = c - (C50 * 50);
             }
-            if ((c >= 20))
+            if ((cantidadTotal >= 50))
             {
-                C20 = (c / 20);
-                c = c - (C20 * 20);
+                b50 = (cantidadTotal / 50);
+                cantidadTotal = cantidadTotal - (b50 * 50);
             }
-            if ((c >= 10))
+            if ((cantidadTotal >= 20))
             {
-                C10 = (c / 10);
-                c = c - (C10 * 10);
+                b20 = (cantidadTotal / 20);
+                cantidadTotal = cantidadTotal - (b20 * 20);
             }
-
+            if ((cantidadTotal >= 10))
+            {
+                b10 = (cantidadTotal / 10);
+                cantidadTotal = cantidadTotal - (b10 * 10);
+            }
         }
     }
 
-    class Program
+    /// <summary>
+    /// Defines the <see cref="Program" />.
+    /// </summary>
+    internal class Program
     {
         // El siguiente programa calcula la cantidad necesaria de billetes de
         // cada tipo para una cantidad dada.
         // Sólo admite billetes de 50, 20 y 10. Si no es así, se produce una excepción
-        static void Main(string[] args)
+        /// <summary>
+        /// The Main.
+        /// </summary>
+        /// <param name="args">The args<see cref="string[]"/>.</param>
+        internal static void Main(string[] args)
         {
             int cant;
-            bill b = new bill();
-
             string linea;
+
             Console.Write("Introduzca una cantidad: ");
+            
             linea = Console.ReadLine();
             cant = int.Parse(linea);
+            Bill bill = new Bill(cant);
 
             try
             {
-                b.establecercant(cant);
-                Console.WriteLine("BILLETES DE 50 : " + b.C50);
-                Console.WriteLine("BILLETES DE 20 : " + b.C20);
-                Console.WriteLine("BILLETES DE 10 : " + b.C10);
+                Console.WriteLine("BILLETES DE 50 : " + bill.mBillete50);
+                Console.WriteLine("BILLETES DE 20 : " + bill.mBillete20);
+                Console.WriteLine("BILLETES DE 10 : " + bill.mBillete10);
             }
             catch
             {
