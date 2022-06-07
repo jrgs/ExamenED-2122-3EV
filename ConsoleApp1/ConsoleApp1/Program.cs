8ef540c15bcee47a11b4c3971bb3fe4d77d99a32
SCJ-2122 <carpena22041@iesmarenostrum.com>
2022-06-07 19:29:06 +0200

    Pregunta2 y 3 examen 3 evaluacion: SCJ2122

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,42 +6,56 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    // Clase bill
-    // Dada una cantidad, la desglosa en billetes de 50, 20, 10
-    //
-    public class bill
-    {
-        // numero de billetes de 50, 20, 10
-        public int C50, C20, C10;
 
-        // constructor vacio, inicializa los atributos
-        public bill()
+    public class Billetes_Scj2122
+    {
+
+        private int cantidadDiez;
+        private int cantidadCincuenta;
+        private int cantidadVeinte;
+
+
+        public int CantidadCincuenta {  get => cantidadCincuenta; 
+                                        set => cantidadCincuenta = value; }
+        public int CantidadVeinte { get => cantidadVeinte; 
+                                    set => cantidadVeinte = value; }
+        public int CantidadDiez {   get => cantidadDiez; 
+                                   set => cantidadDiez = value; }
+
+
+        public Billetes_Scj2122()
         {
-            C50 = 0;
-            C20 = 0;
-            C10 = 0;
+            CantidadCincuenta = 0;
+            CantidadVeinte = 0;
+            CantidadDiez = 0;
         }
 
-        // este método permite cambiar la cantidad total y recalcular
-        public void establecercant(int c)
+        ///<summary>
+        ///<para>Funcion que sirve para contar y recalcular la cantidad de billetes pasandole una cantidad total </para>
+        ///<param name="cantidad"> La variable cantidad hace referencia a la cantidad total que posteriormente se desglosara en billetes</param>
+        ///<returns>devuelve la cantidad de billetes de cada tipo que ha contabilizado dandole valor a los miembros de la clase</returns>
+        ///<exception>Devuelve excepcion cuando introducimos un valor que al dividir por 10 su resto no sea 0</exception>
+        ///</summary>
+        ///<remarks></remarks>
+        public void EstablecerCantidadTotal(int cantidad)
         {
-            if ((c % 10) != 0)
+            if ((cantidad % 10) != 0)
                 throw new ArgumentOutOfRangeException();
 
-            if ((c >= 50))
+            if ((cantidad >= 50))
             {
-                C50 = (c / 50);
-                c = c - (C50 * 50);
+                CantidadCincuenta = cantidad / 50;
+                cantidad = cantidad - (CantidadCincuenta * 50);
             }
-            if ((c >= 20))
+            if ((cantidad >= 20))
             {
-                C20 = (c / 20);
-                c = c - (C20 * 20);
+                CantidadVeinte = cantidad / 20;
+                cantidad = cantidad - (CantidadVeinte * 20);
             }
-            if ((c >= 10))
+            if ((cantidad >= 10))
             {
-                C10 = (c / 10);
-                c = c - (C10 * 10);
+                CantidadDiez = cantidad / 10;
+                cantidad = cantidad - (CantidadDiez * 10);
             }
 
         }
@@ -54,20 +68,20 @@ namespace ConsoleApp1
         // Sólo admite billetes de 50, 20 y 10. Si no es así, se produce una excepción
         static void Main(string[] args)
         {
-            int cant;
-            bill b = new bill();
+            int cantidad;
+            Billetes_Scj2122 billetes = new Billetes_Scj2122();
 
             string linea;
             Console.Write("Introduzca una cantidad: ");
             linea = Console.ReadLine();
-            cant = int.Parse(linea);
+            cantidad = int.Parse(linea);
 
             try
             {
-                b.establecercant(cant);
-                Console.WriteLine("BILLETES DE 50 : " + b.C50);
-                Console.WriteLine("BILLETES DE 20 : " + b.C20);
-                Console.WriteLine("BILLETES DE 10 : " + b.C10);
+                billetes.EstablecerCantidadTotal(cantidad);
+                Console.WriteLine("BILLETES DE 50 : " + billetes.CantidadCincuenta);
+                Console.WriteLine("BILLETES DE 20 : " + billetes.CantidadVeinte);
+                Console.WriteLine("BILLETES DE 10 : " + billetes.CantidadDiez);
             }
             catch
             {
--- ConsoleApp1/UnitTestProject1/UnitTest1.cs
+++ ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -8,12 +8,34 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ConteoCorrecto_Test()
         {
             int cantidad = 280;
-            int billetes50 = 4;
-            int billetes20 = 1;
-            int billetes10 = 1;
+            int billetesCincuenta = 5;
+            int billetesVeinte = 1;
+            int billetesDiez = 1;
+
+            Billetes_Scj2122 billetes = new Billetes_Scj2122();
+
+            billetes.EstablecerCantidadTotal(cantidad);
+
+            Assert.AreEqual(billetesCincuenta, billetes.CantidadCincuenta, 0.001, "La cantidad de billetes 50 no es correcta");
+            Assert.AreEqual(billetesVeinte, billetes.CantidadVeinte, 0.001, "La cantidad de billetes 20 no es correcta");
+            Assert.AreEqual(billetesDiez, billetes.CantidadDiez, 0.001, "La cantidad de billetes 10 no es correcta");
+
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CantidadIntroducidaIncorrecta_Test()
+        {
+            int cantidad = 253;
+
+            Billetes_Scj2122 billetes = new Billetes_Scj2122();
+
+            billetes.EstablecerCantidadTotal(cantidad);
+
+        }
+
     }
 }
