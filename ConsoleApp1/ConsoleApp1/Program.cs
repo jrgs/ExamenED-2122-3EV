2bfca4ddd776c94fbec1710bb19d059b8ca62b1d
demadaria21478 <demadaria21478@AULA1418>
2022-06-07 19:39:53 +0200

    Cambios Realizados en Examen Pablo de Madaria Pascual

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,42 +6,101 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    // Clase bill
-    // Dada una cantidad, la desglosa en billetes de 50, 20, 10
-    //
-    public class bill
-    {
-        // numero de billetes de 50, 20, 10
-        public int C50, C20, C10;
 
-        // constructor vacio, inicializa los atributos
-        public bill()
-        {
-            C50 = 0;
-            C20 = 0;
-            C10 = 0;
+    //PDMP-2122
+    /// <summary>
+    /// Dada una cantidad, la desglosa en billetes de 50, 20, 10
+    /// </summary>
+    /// <remarks> La cantiad debe ser mayor o igual a 10</remarks>
+    public class DesgloseBilletes
+    {
+        /// <summary>
+        /// Miembro Numero de billetes de 50
+        /// </summary>
+        private int numeroBilletes50;
+        /// <summary>
+        /// Miembro Numero de billetes de 20
+        /// </summary>
+        private int numeroBilletes20;
+        /// <summary>
+        /// Miembro Numero de billetes de 10
+        /// </summary>
+        private int numeroBilletes10;
+        /// <summary>
+        /// Valor de la constante 50
+        /// </summary>
+        private const int valorBillete50 = 50;
+        /// <summary>
+        /// Valor de la constante 20
+        /// </summary>
+        private const int valorBillete20 = 20;
+        /// <summary>
+        /// Valor de la constante 10
+        /// </summary>
+        private const int valorBillete10 = 10;
+
+        /// <summary>
+        /// Propiedad numero de billetes de 50
+        /// </summary>
+        /// <value>Numero Billetes 50</value>
+        public int NumeroBilletes50 
+        { 
+            get => numeroBilletes50; 
+            set => numeroBilletes50 = value; 
+        }
+        /// <summary>
+        /// Propiedad numero de billetes de 20
+        /// </summary>
+        /// <value>Numero Billetes 20</value>
+        public int NumeroBilletes20 
+        { 
+            get => numeroBilletes20; 
+            set => numeroBilletes20 = value; 
+        }
+        /// <summary>
+        /// Propiedad numero de billetes de 10
+        /// </summary>
+        /// <value>Numero Billetes 10</value>
+        public int NumeroBilletes10 
+        { 
+            get => numeroBilletes10; 
+            set => numeroBilletes10 = value; 
         }
 
-        // este método permite cambiar la cantidad total y recalcular
-        public void establecercant(int c)
+        /// <summary>
+        /// Constructor inicializacion a 0
+        /// </summary>
+        public DesgloseBilletes()
         {
-            if ((c % 10) != 0)
+            NumeroBilletes50 = 0;
+            NumeroBilletes20 = 0;
+            NumeroBilletes10 = 0;
+        }
+
+       /// <summary>
+       /// Calcula la cantidad de billetes de 50 20 y 10
+       /// </summary>
+       /// <param name="importe">Valor entero y debe ser mayor o igual a 10</param>
+        public void CambioCantidadYRecalculo(int importe)
+        {
+            if ((importe % valorBillete10) != 0)
+                ///<exception cref="System.ArgumentOutOfRangeException">Cuando el importe es menor a 10</exception>
                 throw new ArgumentOutOfRangeException();
 
-            if ((c >= 50))
+            if ((importe >= valorBillete50))
             {
-                C50 = (c / 50);
-                c = c - (C50 * 50);
+                NumeroBilletes50 = (importe / valorBillete50);
+                importe = importe - (NumeroBilletes50 * valorBillete50);
             }
-            if ((c >= 20))
+            if ((importe >= valorBillete20))
             {
-                C20 = (c / 20);
-                c = c - (C20 * 20);
+                NumeroBilletes20 = (importe / valorBillete20);
+                importe = importe - (NumeroBilletes20 * valorBillete20);
             }
-            if ((c >= 10))
+            if ((importe >= valorBillete10))
             {
-                C10 = (c / 10);
-                c = c - (C10 * 10);
+                NumeroBilletes10 = (importe / valorBillete10);
+                importe = importe - (NumeroBilletes10 * valorBillete10);
             }
 
         }
@@ -52,10 +111,12 @@ namespace ConsoleApp1
         // El siguiente programa calcula la cantidad necesaria de billetes de
         // cada tipo para una cantidad dada.
         // Sólo admite billetes de 50, 20 y 10. Si no es así, se produce una excepción
+        
+        
         static void Main(string[] args)
         {
             int cant;
-            bill b = new bill();
+            DesgloseBilletes b = new DesgloseBilletes();
 
             string linea;
             Console.Write("Introduzca una cantidad: ");
@@ -64,12 +125,12 @@ namespace ConsoleApp1
 
             try
             {
-                b.establecercant(cant);
-                Console.WriteLine("BILLETES DE 50 : " + b.C50);
-                Console.WriteLine("BILLETES DE 20 : " + b.C20);
-                Console.WriteLine("BILLETES DE 10 : " + b.C10);
+                b.CambioCantidadYRecalculo(cant);
+                Console.WriteLine("BILLETES DE 50 : " + b.NumeroBilletes50);
+                Console.WriteLine("BILLETES DE 20 : " + b.NumeroBilletes20);
+                Console.WriteLine("BILLETES DE 10 : " + b.NumeroBilletes10);
             }
-            catch
+            catch(ArgumentOutOfRangeException)
             {
                 Console.WriteLine("Cantidad no válida.");
             }
--- ConsoleApp1/UnitTestProject1/UnitTest1.cs
+++ ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -1,19 +1,50 @@
 ﻿using System;
 using ConsoleApp1;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+using System;
 
+
+//PDMP2122
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValoresCorrectosResultadoEsperado()
         {
             int cantidad = 280;
-            int billetes50 = 4;
+            int billetes50 = 5;
             int billetes20 = 1;
             int billetes10 = 1;
+
+            DesgloseBilletes desglose = new DesgloseBilletes();
+
+            desglose.CambioCantidadYRecalculo(cantidad);
+
+            int actualBilletes50 = desglose.NumeroBilletes50;
+            int actualBilletes20 = desglose.NumeroBilletes20;
+            int actualBilletes10 = desglose.NumeroBilletes10;
+            
+            Assert.AreEqual(billetes50, actualBilletes50);
+            Assert.AreEqual(billetes20, actualBilletes20);
+            Assert.AreEqual(billetes10, actualBilletes10);
+            
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ImporteIncorrectoSeEsperaExcepcion()
+        {
+            int cantidad = 8;
+            
+            DesgloseBilletes desglose = new DesgloseBilletes();
+
+            desglose.CambioCantidadYRecalculo(cantidad);
+
+        }
+
+
     }
 }
